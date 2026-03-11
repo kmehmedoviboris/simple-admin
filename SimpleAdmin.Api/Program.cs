225207e573eafb98147b367edf2e14f54d826c83
Kenan Mehmedov <kmehmedov@iboris.eu>
2026-03-11 11:57:54 +0200

    feat(01-02): configure OpenIddict server, validation, authentication, CORS, and Razor Pages
    - Added OpenIddict with AuthorizationCode + PKCE, passthrough mode, dev certs, disabled token encryption
    - Set DefaultScheme to OpenIddictValidationAspNetCoreDefaults (not JwtBearer)
    - Cookie scheme as DefaultChallengeScheme with /Account/Login path
    - CORS policy for http://localhost:5173
    - Middleware order: UseRouting > UseCors > UseAuthentication > UseAuthorization > MapControllers > MapRazorPages

--- SimpleAdmin.Api/Program.cs
+++ SimpleAdmin.Api/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
+using OpenIddict.Validation.AspNetCore;
 using SimpleAdmin.Api.Data;
 using SimpleAdmin.Api.Models;
+using SimpleAdmin.Api.Workers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,11 +24,65 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
+builder.Services.AddOpenIddict()
+    .AddCore(options =>
+    {
+        options.UseEntityFrameworkCore()
+               .UseDbContext<ApplicationDbContext>();
+    })
+    .AddServer(options =>
+    {
+        options.SetAuthorizationEndpointUris("/connect/authorize")
+               .SetTokenEndpointUris("/connect/token");
+        options.AllowAuthorizationCodeFlow()
+               .RequireProofKeyForCodeExchange();
+        options.AddDevelopmentEncryptionCertificate()
+               .AddDevelopmentSigningCertificate();
+        options.DisableAccessTokenEncryption();
+        options.UseAspNetCore()
+               .EnableAuthorizationEndpointPassthrough()
+               .EnableTokenEndpointPassthrough()
+               .DisableTransportSecurityRequirement();
+    })
+    .AddValidation(options =>
+    {
+        options.UseLocalServer();
+        options.UseAspNetCore();
+    });
+
+// DefaultScheme MUST be OpenIddictValidationAspNetCoreDefaults (not JwtBearer, not Cookie)
+// Cookie is only the DefaultChallengeScheme for login redirects
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+})
+.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
+{
+    options.LoginPath = "/Account/Login";
+});
+
+builder.Services.AddAuthorization();
+builder.Services.AddRazorPages();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 builder.Services.AddControllers();
+builder.Services.AddHostedService<OpenIddictWorker>();
 
 var app = builder.Build();
 
 app.UseRouting();
+app.UseCors();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
+app.MapRazorPages();
 
 app.Run();
